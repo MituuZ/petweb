f6a16dd654d24c5abd96bfdc6219dd7f8cfe60ac
Mitja Leino <mituuuuz@hotmail.com>
2024-03-23 09:26:44 +0200

    Add Flask page and basic web items

--- petweb.py
+++ petweb.py
@@ -1,4 +1,15 @@
 from db.setup_database import setup_database
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     setup_database()
+    app.run(host='0.0.0.0', port=8080)
