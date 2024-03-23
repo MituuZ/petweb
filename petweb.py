eb5b40009ba65d4959bbc39900e1fbe0ffaf6b98
Mitja Leino <mituuuuz@hotmail.com>
2024-03-23 09:37:28 +0200

    Add statistics page and missing styles

--- petweb.py
+++ petweb.py
@@ -10,6 +10,11 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/statistics')
+def statistics():
+    return render_template('statistics.html')
+
+
 if __name__ == '__main__':
     setup_database()
     app.run(host='0.0.0.0', port=8080)
