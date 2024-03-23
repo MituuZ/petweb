3325853a086f8968f69b81094f22bd2bc64e6346
Mitja Leino <mituuuuz@hotmail.com>
2024-03-23 09:15:00 +0200

    Add insertion and second table

--- db/setup_database.py
+++ db/setup_database.py
@@ -1,17 +1,22 @@
 import sqlite3
+from sqlite3 import Cursor
 
 try:
     from config.config import Config
 except ImportError:
     from config.config_example import Config
 
+cursor: Cursor
+
 
 def setup_database():
+    global cursor
+
     print("Setting up database to: ", Config.DATABASE_PATH)
     try:
         conn = sqlite3.connect(Config.DATABASE_PATH + 'petweb.db')
         cursor = conn.cursor()
-        create_tables(cursor)
+        create_tables()
 
         res = cursor.execute("SELECT name FROM sqlite_master")
         print(res.fetchone())
@@ -19,10 +24,26 @@ def setup_database():
         print("Error setting up database: ", e)
 
 
-def create_tables(cursor):
+def create_tables():
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS pet_weights (
         name TEXT NOT NULL, 
         weight REAL NOT NULL, 
-        date date DEFAULT CURRENT_DATE)
+        date date DEFAULT CURRENT_DATE,
+        UNIQUE(name, date))
     """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS pets (
+        name TEXT NOT NULL, 
+        species TEXT NOT NULL, 
+        birth_day date, 
+        UNIQUE(name))
+    """)
+
+
+def insert_into_pet_weights(name, weight):
+    cursor.execute("INSERT INTO pet_weights (name, weight) VALUES (?, ?)", (name, weight))
+    res = cursor.execute("SELECT * FROM pet_weights")
+    cursor.connection.commit()
+    print(res.fetchall())
